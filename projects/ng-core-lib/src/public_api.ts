a7627af824de61be81d282d82e125934d4c5d615
tonomony <tonomony@icloud.com>
2018-11-16 14:21:52 +0100

    importing services separatly

--- projects/ng-core-lib/src/public_api.ts
+++ projects/ng-core-lib/src/public_api.ts
@@ -2,5 +2,5 @@
  * Public API Surface of ng-core-lib
  */
 
-export * from './lib/services/loading.service';
-export * from './lib/ng-core-lib.module';
+ export * from './lib/ng-core-lib.module';
+ export * from './lib/services';
